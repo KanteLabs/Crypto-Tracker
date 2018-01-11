bad994686d8b54de7c582169d40528d12fc87758
Amidou Kante <kamdiou@gmail.com>
2018-01-11 16:21:49 -0500

    created example file

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var api = {}
 
+api.getByCoin(coin, callback) = function(){
+    console.log(coin)
+}
+
 module.exports = api;
--- example.js
+++ example.js
@@ -1,1 +1,5 @@
 var api = require('./app')
+
+api.getByCoin("eth", (req, res, err)=>{
+    console.log(...args)
+})
