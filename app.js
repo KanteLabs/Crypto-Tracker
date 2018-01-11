f1119ae849de2bbf419bd330bf3f4b4c5fb74fc9
Amidou Kante <kamdiou@gmail.com>
2018-01-11 16:39:53 -0500

    Require request

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const axios = require('axios');
-var api = {}
+const axios = require("axios");
+var api = {};
 
 api.getByCoin = (options, callback) => {
     let coin = options.coin;
     let currency = options.currency;
     let url = `https://api.coinmarketcap.com/v1/ticker/${coin}/?convert=${currency}`;
 
-    request.get(url, (err, res, body)=>{
-       let data = JSON.parse(body);
-       
+    axios.get(url).then((res)=>{
+        console.log(res)
+    }).catch((err)=>{
+        console.log(err)
     })
-
     return callback(data, coin, currency)
 }
 
