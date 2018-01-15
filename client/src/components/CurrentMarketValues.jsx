126741b229ba6f739d3283b0c88920cb248fa697
Amidou Kante <kamdiou@gmail.com>
2018-01-14 22:24:08 -0500

    can display current market values

--- client/src/components/CurrentMarketValues.jsx
+++ client/src/components/CurrentMarketValues.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const CurrentMarketValues = (props) => {
-    
+    const {data} = props;
     return(
-        <h1>Hi</h1>
+        <div id="current-market">
+            <ul>
+                {data.map((coin, i)=>{
+                    return(
+                        <li key={i}>
+                            <p className="name">Rank: {coin.rank} - {coin.name}({coin.symbol})</p>
+                            <p className="price">USD: ${coin.price_usd}</p>
+                            <p className="price-change">1hr Price Change: {coin.percent_change_1h}%</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
 
