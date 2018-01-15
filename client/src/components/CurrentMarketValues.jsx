a52bdfacf30d58c27d81edace0fef444e2a70de9
Amidou Kante <kamdiou@gmail.com>
2018-01-14 22:31:05 -0500

    updated values to be display from api call

--- client/src/components/CurrentMarketValues.jsx
+++ client/src/components/CurrentMarketValues.jsx
@@ -8,9 +8,14 @@ const CurrentMarketValues = (props) => {
                 {data.map((coin, i)=>{
                     return(
                         <li key={i}>
-                            <p className="name">Rank: {coin.rank} - {coin.name}({coin.symbol})</p>
-                            <p className="price">USD: ${coin.price_usd}</p>
-                            <p className="price-change">1hr Price Change: {coin.percent_change_1h}%</p>
+                            <div className="top-text">
+                                <p className="name">Rank: {coin.rank} - {coin.name}({coin.symbol})</p>
+                                <p className="price-change">1hr Price Change: {coin.percent_change_1h}%</p>
+                            </div>
+                            <div className="bottom-text">
+                                <p className="market-cap">Market Cap USD: ${coin.market_cap_usd}</p>
+                                <p className="price">USD: ${coin.price_usd}</p>
+                            </div>
                         </li>
                     )
                 })}
