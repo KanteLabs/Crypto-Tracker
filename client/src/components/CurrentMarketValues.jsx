ea00dfa57b6e120f7f3f99aa100acbc97e50899e
Amidou Kante <kamdiou@gmail.com>
2018-01-14 21:45:26 -0500

    component to get current market values

---
+++ client/src/components/CurrentMarketValues.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CurrentMarketValues extends Component{
+    render(){
+        return(
+            <h1>Hi</h1>
+        )
+    }
+}
+
+export default CurrentMarketValues;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CurrentMarketValues from './CurrentMarketValues';
 
 class Home extends Component{
     render(){
