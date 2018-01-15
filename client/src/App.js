fcc5b9c253063296815a957f619e14622a8256be
Amidou Kante <kamdiou@gmail.com>
2018-01-14 21:07:11 -0500

    state to track drawer status

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,20 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Navbar from './components/Navbar';
+import Drawer from './components/Drawer';
 import firebase from './config/firebase';
 import './App.css';
 
 var db = firebase.firestore();
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      drawerStatus: false
+    }
+  }
+
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user)=>{
       if (user) {
@@ -18,10 +26,19 @@ class App extends Component {
       }
     })
   }
+
+  changeDrawerStatus = (status) => {
+    console.log(status);
+    this.setState({
+      drawerStatus: status
+    })
+  }
+
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        <Navbar />
+        <Navbar changeDrawerStatus={(status)=>this.changeDrawerStatus(status)}/>
+        <Drawer openState={this.state.drawerStatus}/>
       </MuiThemeProvider>
     );
   }
--- client/src/components/Drawer.js
+++ client/src/components/Drawer.js
@@ -3,8 +3,16 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
 
-class Drawer extends component {
-
+class Drawer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+    componentWillReceiveProps(prev, next){
+        console.log(prev, next)
+    }
 }
 
 export default Drawer;
