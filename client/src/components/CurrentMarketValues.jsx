e1217d81bb8da3d8db443211ee12e567756fb155
Amidou Kante <kamdiou@gmail.com>
2018-01-14 21:48:24 -0500

    function to grab top 5 market data

--- client/src/components/CurrentMarketValues.jsx
+++ client/src/components/CurrentMarketValues.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class CurrentMarketValues extends Component{
+
+    componentDidMount() {
+        let url = 'https://api.coinmarketcap.com/v1/ticker/?limit=5';
+
+        fetch(url).then((res)=>(
+            console.log(res.json())
+        )).catch((err)=>(
+            console.log(err)
+        ))
+    }
+    
     render(){
         return(
             <h1>Hi</h1>
