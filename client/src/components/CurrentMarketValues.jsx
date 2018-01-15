c0ee683d848c42ae2079df931c7abb379a4ee800
Amidou Kante <kamdiou@gmail.com>
2018-01-14 21:58:45 -0500

    request current market values in home

--- client/src/components/CurrentMarketValues.jsx
+++ client/src/components/CurrentMarketValues.jsx
@@ -1,24 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class CurrentMarketValues extends Component{
-
-    componentDidMount() {
-        let url = 'https://api.coinmarketcap.com/v1/ticker/?limit=5';
-
-        fetch(url).then((res)=>{
-            res.json().then((data)=>{
-                console.log(data)
-            }).catch(err=>console.log(err))
-        }).catch((err)=>{
-            console.log(err)
-        })
-    }
+const CurrentMarketValues = (props) => {
     
-    render(){
-        return(
-            <h1>Hi</h1>
-        )
-    }
+    return(
+        <h1>Hi</h1>
+    )
 }
 
 export default CurrentMarketValues;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 import CurrentMarketValues from './CurrentMarketValues';
 
 class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            topFiveCoins: false,
+            topFiveCoinsLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        
+    }
+
+    grabTopFiveCoins(){
+        let url = 'https://api.coinmarketcap.com/v1/ticker/?limit=5';
+
+        if(!this.state.topFiveCoinsLoaded){
+            fetch(url).then((res)=>{
+                res.json().then((data)=>{
+                    console.log(data)
+                    this.setState({
+                        topFiveCoins: data,
+                        topFiveCoinsLoaded: true
+                    })
+                }).catch(err=>console.log(err))
+            }).catch((err)=>{
+                console.log(err)
+            })
+        }else{
+
+        }
+    }
+
     render(){
         return(
             <section className="home">
-                <CurrentMarketValues />
+                <CurrentMarketValues data={this.state.topFiveCoins}/>
             </section>
         )
     }
