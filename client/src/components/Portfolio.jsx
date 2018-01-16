cd80dddee4d0ee46f3fa0a1a8c36a4293a8366f7
Amidou Kante <kamdiou@gmail.com>
2018-01-16 12:57:53 -0500

    added redirect for users not signed in'

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
+import firebase from '../config/firebase';
+import { Redirect } from 'react-router-dom';
+
+var db = firebase.firestore();
 
 class Portfolio extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+            userUID: false,
+            redirect: false,
+            page: ''
         }
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user)=>{
+            if(user){
+                this.setState({
+                    userID: user.uid
+                })
+            }else{
+                this.setState({
+                    redirect: true,
+                    page:'/login'
+                })
+            }
+        })
     }
     
     render(){
+        const {redirect, page} = this.state;
         return(
             <section id="portfolio">
-                
+                {redirect ? <Redirect to={page} />: null}
             </section>
         )
     }
