c44d19f6e73ef44f8757ba4e3103829c43b430bb
Amidou Kante <kamdiou@gmail.com>
2018-01-16 10:48:05 -0500

    red or green on price change

--- client/src/components/CurrentMarketValues.jsx
+++ client/src/components/CurrentMarketValues.jsx
@@ -11,7 +11,7 @@ const CurrentMarketValues = (props) => {
                         <li key={i}>
                             <div className="top-text">
                                 <p className="name">{coin.name}({coin.symbol})</p>
-                                <p className="price-change">{coin.percent_change_1h}%</p>
+                                <p className="price-change" style={{color: coin.percent_change_1h > 0 ? 'green' : 'red'}}>{coin.percent_change_1h}%</p>
                             </div>
                             <div className="bottom-text">
                                 <p className="price-btc">Price BTC: {coin.price_btc}</p>
