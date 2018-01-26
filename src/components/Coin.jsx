50933f2ba976b90906bb015c555df4bf5aa16ea0
Amidou Kante <kamdiou@gmail.com>
2018-01-26 13:20:54 -0500

    updated components strucutre

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Coin = (props) => {
     const {coin, x} = props;
     return(
-        <li className="coin" id={coin.coin} x={x}>
+        <li className="coin" id={coin.coin} key={x}>
+            {coin.amount}
         </li>
     )
 }
