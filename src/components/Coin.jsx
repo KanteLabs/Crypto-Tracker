17de2fa8af0a7923443219949b2c6d61c4df98cc
Amidou Kante <kamdiou@gmail.com>
2018-01-26 16:27:53 -0500

    changed tracked coin into a class component

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -4,7 +4,14 @@ const Coin = (props) => {
     const {coin, x} = props;
     return(
         <li className="coin" id={coin.coin} key={x}>
-            {coin.amount}
+            <div className="top-text">
+                <p className="name">{coin.coin.textKey} ({coin.coin.valueKey})</p>
+                {/* <p className="price-change" style={{color: coin.percent_change_1h > 0 ? 'green' : 'red'}}>{coin.percent_change_1h}%</p> */}
+            </div>
+            <div className="bottom-text">
+                {/* <p className="price-btc">Price BTC: {coin.price_btc}</p> */}
+                {/* <p className="price">USD: ${coin.price_usd}</p> */}
+            </div>
         </li>
     )
 }
--- src/components/TrackedCoins.jsx
+++ src/components/TrackedCoins.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Coin from './Coin';
 import firebase from '../config/firebase';
 var db = firebase.firestore();
 
-const TrackedCoins = (props) => {
-    const { coinData } = props;
-    return(
-        <div id="tracked-coins">
-            {Object.keys(coinData).map((coin, i)=>{
-                console.log(coin, coinData[coin]) 
-                return(
-                    <ul key={i} id={`coin-list ${coin}`}>
-                        {Object.values(coinData[coin]).map((item, x)=>{
-                            return <Coin coin={item} key={x} x={x} />
-                        })}
-                    </ul>
-                )
-            })}
-        </div>
-    )
+class TrackedCoins extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        const { coinData } = this.props;
+        return(
+            <div id="tracked-coins">
+                {Object.keys(coinData).map((coin, i)=>{
+                    console.log(coin, coinData[coin]) 
+                    return(
+                        <ul key={i} id={`coin-list ${coin}`}>
+                            {Object.values(coinData[coin]).map((item, x)=>{
+                                return <Coin coin={item} key={x} x={x} />
+                            })}
+                        </ul>
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
 export default TrackedCoins;
