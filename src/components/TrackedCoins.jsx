3425f9ff52dafc0a836f5b152d2c42de1992ab06
Amidou Kante <kamdiou@gmail.com>
2018-01-26 16:52:45 -0500

    grabs apu data for each owned coin

--- src/components/TrackedCoins.jsx
+++ src/components/TrackedCoins.jsx
@@ -7,16 +7,48 @@ class TrackedCoins extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+            coinDataLoaded: false
         }
     }
+    
+    shouldComponentUpdate(prev, next){
+        if(Object.keys(next).length === 0){
+            return false;
+        }else if(next.coinDataLoaded == this.state.coinDataLoaded){
+            return false
+        }else{
+            return true;
+        }
+    }
+
+    grabCoinData = (coin) => {
+        let url = `https://api.coinmarketcap.com/v1/ticker/${coin}/?convert=USD`;
+        fetch(url).then((res)=>{
+            res.json().then((data)=>{
+                console.log(data)
+                this.setState({
+                    [coin]: data,
+                    coinDataLoaded: true
+                })
+                return data;
+            }).then(()=>{
+                setTimeout(this.grabCoinData, 300000);
+            })
+            .catch(err=>console.log(err))
+            return res;
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+
 
     render(){
         const { coinData } = this.props;
         return(
             <div id="tracked-coins">
                 {Object.keys(coinData).map((coin, i)=>{
-                    console.log(coin, coinData[coin]) 
+                    this.grabCoinData(coin)
+                    console.log("cat")
                     return(
                         <ul key={i} id={`coin-list ${coin}`}>
                             {Object.values(coinData[coin]).map((item, x)=>{
